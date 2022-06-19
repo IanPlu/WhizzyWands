e2931786fdf3207c375a72ce56a92ea9ccd03545
IanPlu <personaltroll@gmail.com>
2022-06-18 21:42:40 -0700

    Renaming everything

--- src/main/java/ianplu/whizzards/client/WhizzardsClient.java
+++
@@ -1,9 +0,0 @@
-package ianplu.whizzards.client;
-
-import net.fabricmc.api.ClientModInitializer;
-
-@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
-public class WhizzardsClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {}
-}
